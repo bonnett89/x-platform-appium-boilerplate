1e5c5be9e00ca09f355fc3373565b48b06ba5919
James Bonnett <james.bonnett@chelsea-apps.com>
2017-05-17 16:46:32 +0100

    Add ruby methods to steps

--- features/step_definitions/hello_world_steps.rb
+++ features/step_definitions/hello_world_steps.rb
@@ -2,7 +2,12 @@
 # Step definitions specific to the feature file 'example.feature' go here.
 ##
 Given(/^I am on the Home screen$/) do
+  @page = launch_to_home_page
 end
 
-Then(/^I should see "(.*?)"$/) do
+Then(/^I should see "(.*?)"$/) do |expected_text|
+  actual_text = @page.hello_world_text
+  unless actual_text == expected_text
+    raise "Saw #{actual_text} instead of #{expected_text}"
+  end
 end
