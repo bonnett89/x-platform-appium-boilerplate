35472efea6bc49aab588e312154feaa2c7e1a8cc
James Bonnett <james.bonnett@chelsea-apps.com>
2017-05-17 16:40:36 +0100

    Add methods to env file

---
+++ features/support/env.rb
@@ -0,0 +1,25 @@
+require 'appium_lib'
+require 'cucumber/ast'
+
+require_relative 'appium_world'
+require_relative '../pages/page_factory'
+
+raise 'NO PLATFORM NAME environment variable' if ENV['PLATFORM_NAME'].nil?
+
+platform = ENV['PLATFORM_NAME'].downcase
+
+appium_file_path = "./config/#{platform}/appium.txt"
+
+caps = Appium.load_settings(file: appium_file_path, verbose: true)
+
+driver = Appium::Driver.new(caps)
+Appium.promote_appium_methods AppiumWorld
+
+World do
+  page_factory = PageFactory.new(platform, driver)
+  AppiumWorld.new(driver, page_factory)
+end
+
+After do
+  app_close
+end
